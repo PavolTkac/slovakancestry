be97586986bab6929ed23c5a8f575c13e950f6e3
Pavol Tkáč <pavol.tkac@student.tuke.sk>
2021-01-17 20:25:31 +0100

    edit

--- js/app.js
+++ js/app.js
@@ -4,12 +4,27 @@ function changePage() {
   var body = document.getElementsByTagName('body')[0];
   body.classList.remove('main');
   body.classList.add('family-tree');
+
+  var imgCover = document.getElementsByClassName('img-cover')[0];
+  if (imgCover) {
+    var img = imgCover.getElementsByClassName('main-bg');
+    if (img && img[0]) {
+      img[0].setAttribute('src', './images/family-tree.jpg');
+    }
+
+    var content = imgCover.getElementsByClassName('content');
+    if (content && content[0]) {
+      content[0].classList.add('shown')
+    }
+  }
+
   signet.classList.add('hidden');
 }
 
 function play() {
   var audio = document.getElementById("audio");
-  audio.play();
+  audio.volume = 0.1;
+  // audio.play();
   changePage();
 }
 
